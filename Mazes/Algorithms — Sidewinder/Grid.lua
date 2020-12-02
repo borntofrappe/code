a63716168963f09e80d3d71731d68b6fd448ba84
borntofrappe <borntofrappe@gmail.com>
2020-12-02 23:53:49 +0100

    add sidewinder algorithm

---
+++ Mazes/Algorithms — Sidewinder/Grid.lua
@@ -0,0 +1,94 @@
+Grid = {}
+Grid.__index = Grid
+
+function Grid:new()
+  local columns = COLUMNS
+  local rows = ROWS
+  local width = WINDOW_WIDTH - PADDING * 2
+  local height = WINDOW_HEIGHT - PADDING * 2
+
+  local cellWidth = math.floor(width / columns)
+  local cellHeight = math.floor(height / rows)
+
+  local cells = {}
+
+  -- allow each cell to have up to two neighbors
+  -- east / north
+  for column = 1, columns do
+    cells[column] = {}
+    for row = 1, rows do
+      local neighbors = {}
+
+      if row > 1 then
+        table.insert(
+          neighbors,
+          {
+            ["column"] = column,
+            ["row"] = row - 1,
+            ["gates"] = {"up", "down"}
+          }
+        )
+      end
+
+      if column < columns then
+        table.insert(
+          neighbors,
+          {
+            ["column"] = column + 1,
+            ["row"] = row,
+            ["gates"] = {"right", "left"}
+          }
+        )
+      end
+
+      cells[column][row] = Cell:new(column, row, neighbors, cellWidth, cellHeight)
+    end
+  end
+
+  this = {
+    ["columns"] = columns,
+    ["rows"] = rows,
+    ["width"] = width,
+    ["height"] = height,
+    ["cells"] = cells
+  }
+
+  setmetatable(this, self)
+  return this
+end
+
+function Grid:render()
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.setLineWidth(4)
+
+  for column = 1, self.columns do
+    for row = 1, self.rows do
+      self.cells[column][row]:render()
+    end
+  end
+end
+
+function Grid:sidewinder()
+  for row = self.rows, 1, -1 do
+    local visited = {}
+    for column = 1, self.columns do
+      local cell = self.cells[column][row]
+      local connection = cell.neighbors[love.math.random(#cell.neighbors)]
+
+      if connection then
+        table.insert(visited, cell)
+        if connection.gates[1] == "up" then
+          local visitedCell = visited[love.math.random(#visited)]
+          local neighboringCell = self.cells[visitedCell.column][connection.row]
+          visitedCell.gates[connection.gates[1]] = nil
+          neighboringCell.gates[connection.gates[2]] = nil
+          visited = {}
+        else
+          local neighboringCell = self.cells[connection.column][connection.row]
+          cell.gates[connection.gates[1]] = nil
+          neighboringCell.gates[connection.gates[2]] = nil
+        end
+      end
+    end
+  end
+end
--- Mazes/README.md
+++ Mazes/README.md
@@ -49,3 +49,59 @@ In the logic of the specific algorithm, each cell has at most two neighbors:
   ```
 
 With this setup, all that is necessary is to loop through the `cells` table, pick a neighbor from the available set and remove the connected gates.
+
+## Sidewinder
+
+The algorithm works similarly to the binary tree algorithm, which means each cell has still up to two neighbors, east or north. When selecting a neighbor north however, the connection is removed from a cell visited in the current row, and not immediately from the current cell.
+
+In detail, traverse the grid left to right.
+
+```lua
+for row = self.rows, 1, -1 do
+end
+```
+
+For each row, keep track of the visited cells in a dedicated data structure.
+
+```lua
+for row = self.rows, 1, -1 do
+  local visited = {}
+  for column = 1, self.columns do
+    local cell = self.cells[column][row]
+    table.insert(visited, cell)
+  end
+end
+```
+
+When the connection describes a neighbor above the current cell, pick a cell at random from the collection.
+
+```lua
+if connection.gates[1] == "up" then
+  local visitedCell = visited[love.math.random(#visited)]
+end
+```
+
+At this point, remove the gates from the appropriate pair.
+
+```lua
+local neighboringCell = self.cells[visitedCell.column][connection.row]
+
+visitedCell.gates[connection.gates[1]] = nil
+neighboringCell.gates[connection.gates[2]] = nil
+```
+
+Finally reset the value of the data structure so that the script locks the cell in place.
+
+```lua
+visited = {}
+```
+
+Outside of this instance, the connection describes a cell east, and the idea is to remove the gates; just like in the binary tree algorithm.
+
+```lua
+else
+  local neighboringCell = self.cells[connection.column][connection.row]
+  cell.gates[connection.gates[1]] = nil
+  neighboringCell.gates[connection.gates[2]] = nil
+end
+```
