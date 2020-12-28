ff7dd6b9c937c18137aca24943b0a023e5b3e442
borntofrappe <borntofrappe@gmail.com>
2020-12-28 23:24:46 +0100

    add multiple boundaries

--- 2D Ray Casting/Lua and Love2D/Particle.lua
+++ 2D Ray Casting/Lua and Love2D/Particle.lua
@@ -3,7 +3,7 @@ Particle.__index = Particle
 
 function Particle:create(x, y)
   local rays = {}
-  for i = 0, 360 do
+  for i = 0, 360, 5 do
     local angle = math.rad(i)
     table.insert(rays, Ray:create(x, y, angle))
   end
@@ -26,16 +26,30 @@ function Particle:render()
   -- end
 end
 
-function Particle:cast(boundary)
-  local points = {}
+function Particle:cast(boundaries)
+  local pts = {}
   for i, ray in ipairs(self.rays) do
-    local point = ray:cast(boundary)
-    if point then
-      table.insert(points, point)
+    local closestPoint = nil
+    local closestDistance = nil
+    for j, boundary in ipairs(boundaries) do
+      local pt = ray:cast(boundary)
+      if pt then
+        local distance = ((pt.x - self.x) ^ 2 + (pt.y - self.y) ^ 2) ^ 0.5
+        if not closestDistance or distance < closestDistance then
+          closestDistance = distance
+          closestPoint = {
+            ["x"] = pt.x,
+            ["y"] = pt.y
+          }
+        end
+      end
+    end
+    if closestPoint then
+      table.insert(pts, closestPoint)
     end
   end
 
-  return points
+  return pts
 end
 
 function Particle:move(x, y)
--- 2D Ray Casting/Lua and Love2D/main.lua
+++ 2D Ray Casting/Lua and Love2D/main.lua
@@ -10,10 +10,22 @@ function love.load()
   love.window.setTitle("2D Ray Casting")
   love.graphics.setBackgroundColor(0.17, 0.17, 0.17)
 
-  wall = Boundary:create(WINDOW_WIDTH * 3 / 4, WINDOW_HEIGHT / 4, WINDOW_WIDTH * 3 / 4 + 20, WINDOW_HEIGHT * 3 / 4)
-  particle = Particle:create(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+  walls = {}
+  table.insert(walls, Boundary:create(0, 0, WINDOW_WIDTH, 0))
+  table.insert(walls, Boundary:create(WINDOW_WIDTH, 0, WINDOW_WIDTH, WINDOW_HEIGHT))
+  table.insert(walls, Boundary:create(WINDOW_WIDTH, WINDOW_HEIGHT, 0, WINDOW_HEIGHT))
+  table.insert(walls, Boundary:create(0, WINDOW_HEIGHT, 0, 0))
 
-  points = particle:cast(wall)
+  for i = 1, 5 do
+    local x1 = love.math.random(WINDOW_WIDTH)
+    local x2 = love.math.random(WINDOW_WIDTH)
+    local y1 = love.math.random(WINDOW_HEIGHT)
+    local y2 = love.math.random(WINDOW_HEIGHT)
+    table.insert(walls, Boundary:create(x1, y1, x2, y2))
+  end
+
+  particle = Particle:create(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+  points = particle:cast(walls)
 end
 
 function love.keypressed(key)
@@ -26,12 +38,15 @@ function love.update()
   local x, y = love.mouse:getPosition()
   if x ~= 0 and y ~= 0 then
     particle:move(x, y)
-    points = particle:cast(wall)
+    points = particle:cast(walls)
   end
 end
 
 function love.draw()
-  wall:render()
+  for i, wall in ipairs(walls) do
+    wall:render()
+  end
+
   particle:render()
 
   if #points > 0 then
