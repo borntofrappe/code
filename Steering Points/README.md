b8b9c5ead99fbf1d88eb979926b3b300392f09ea
borntofrappe <borntofrappe@gmail.com>
2021-01-27 23:53:03 +0100

    code | read grid

--- Steering Points/README.md
+++ Steering Points/README.md
@@ -1,10 +1,20 @@
+<!-- TODOS
+- [x] Create grid
+  - [ ] rename project to 'Create grid'
+- [x] Read grid
+- [ ] Make points into particles subject to multiple forces
+  - [ ] attracted to original position
+  - [ ] repelled by mouse cursor
+  - [ ] repelled by other particles
+ -->
+
 # Steering Points
 
 The project is inspired by [a coding challenge](https://youtu.be/4hA7G3gup-4) from [The Coding Train](https://thecodingtrain.com/), and might actually involve a series of demos to ultimately create a grid of points tracing an outline. The idea is to ultimately have particles trace the outline of a shape, and have them scatter as the mouse cursor approaches each and every one of them.
 
 ## Create grid
 
-The first demo sets up a grid with a fixed number of columns and rows. This grid is modified with mouse and keyboard input by:
+The demo sets up a grid with a fixed number of columns and rows. This grid is modified with mouse and keyboard input by:
 
 - clicking
 
@@ -23,3 +33,13 @@ The first demo sets up a grid with a fixed number of columns and rows. This grid
   - `p` to create a `.png` image
 
 _Please note:_ the `.txt` and `.png` files are produced in the default path described by [`love.system`](https://love2d.org/wiki/love.filesystem)
+
+## Read grid
+
+Starting from a string of `x`s and `o`s, much similar to one created with the previous demo, the goal is to populate a grid with points matching the second character.
+
+It is important to mention that the width and height of the window are set only after the script is able to discern the number of columns and rows.
+
+For the number columns the position of the first newline character `\n` gives the index of the character separating the first and second row.
+
+For the number of rows, instead, the number of newline characters `\n` points to one less than the actual value (since the last row ends with an `x` or `o`).
---
+++ Steering Points/Read grid/main.lua
@@ -0,0 +1,125 @@
+require "Grid"
+
+SEGMENTS = 20
+STRING =
+  [[xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxoooxxxoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxoooooxooooxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxooooooooooxxxxxxxxoxxxxxoxxxxxxxxxxxxxxx
+xxxxxxxxxxxxoooooooooooxxxxooxxxxxxxxooxxxxxxxxxxxxxx
+xxxxxxxxxxoxxxoxoooooooxxxxoxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxooxxooooooooooxxxxxxxxxooxxxxooxxxxxxxxxxxxx
+xxxxxxxxoooxoooxxxooooxxxxxxxxxxoxxxooooxxxooxxxxxxxx
+xxxxxxxxoooooooxxxooooxxxxxxxxxxoxxoooooooxooxxxxxxxx
+xxxoooxxxxooxoooxxoooxxxxxxxxoxxxxooooooooooooooooxxx
+xxxooooooooooooxxxoooxxoxxxoooooxoooooooooooooooooxxx
+xxxxooooooooooxxxxooxxooxoooooooooooooooooooooooxxxxx
+xxxooooooooooxxoxxoxxxxxxoooxoooooooooooooooooooxxxxx
+xxxooxxoooooooxooxxxxxxxoxxxoooooooooooooooooooxxxxxx
+xxxxxxxxooooooooooxxxxxxxoooooooooooooooooooxooxxxxxx
+xxxxxxxxooooooooooxxxxxxxoooooooooooooooooooxooxxxxxx
+xxxxxxxxoooooooooxxxxxxxooxooxoxoooooooooooxoxoxxxxxx
+xxxxxxxxxooooooxxxxxxxxxoxxoooooooooooooooxooxxxxxxxx
+xxxxxxxxxxoooooxxxxxxxxoooooxxoxoooooooooxooxxxxxxxxx
+xxxxxxxxxxxooxxooxxxxxoooooooooooxoooooooxxxxxxxxxxxx
+xxxxxxxxxxxoooxxxxxxxxooooooooooxxxooxooxxxxxxxxxxxxx
+xxxxxxxxxxxxxxooooxxxxxoooooooooxxxoxxxxxoxxxxxxxxxxx
+xxxxxxxxxxxxxxoooooxxxxxxxooooooxxxxxooxoxxoxxxxxxxxx
+xxxxxxxxxxxxxxooooooxxxxxxoooooxxxxxxxoooxxooxxxxxxxx
+xxxxxxxxxxxxxxooooooxxxxxxooooxxxxxxxxxxxxooxxxxxxxxx
+xxxxxxxxxxxxxxxooooxxxxxxxooooxoxxxxxxxxxooooxxxxxxxx
+xxxxxxxxxxxxxxxooooxxxxxxxxooxxoxxxxxxxxooooooxxxxxxx
+xxxxxxxxxxxxxxxoooxxxxxxxxxxoxxxxxxxxxxxooooooxxxxxxx
+xxxxxxxxxxxxxxxoooxxxxxxxxxxxxxxxxxxxxxxoxxooxxxoxxxx
+xxxxxxxxxxxxxxxooxxxxxxxxxxxxxxxxxoxxxxxxxxxxxxoxxxxx
+xxxxxxxxxxxxxxxoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx]]
+
+function love.load()
+  local columns = STRING:find("\n") - 1
+  local string, rows = STRING:gsub("\n", "")
+  rows = rows + 1
+
+  CELL_SIZE = 18
+  PADDING = 2
+  RADIUS = CELL_SIZE / 2 - PADDING
+
+  WINDOW_WIDTH = CELL_SIZE * columns
+  WINDOW_HEIGHT = CELL_SIZE * rows
+
+  love.window.setTitle("Steering points - Read grid")
+  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
+  love.graphics.setBackgroundColor(0.17, 0.17, 0.17)
+
+  grid = Grid:new(columns, rows)
+
+  for i = 1, #string do
+    if string:sub(i, i) == "o" then
+      local column = ((i - 1) % columns) + 1
+      local row = math.floor(i / columns) + 1
+      grid:addPoint(column, row)
+    end
+  end
+end
+
+function love.keypressed(key)
+  local key = key:lower()
+  if key == "e" then
+    grid:removePoints()
+  elseif key == "g" then
+    grid:toggleGridLines()
+  elseif key == "t" then
+    local characters = {}
+    for i = 1, COLUMNS * ROWS do
+      characters[#characters + 1] = "x"
+    end
+
+    for i, point in pairs(grid.points) do
+      local column = math.floor(point.x / CELL_SIZE) + 1
+      local row = math.floor(point.y / CELL_SIZE) + 1
+
+      local index = column + (row - 1) * COLUMNS
+      characters[index] = "o"
+    end
+
+    for r = 1, ROWS - 1 do
+      local index = (COLUMNS + 1) * r
+      table.insert(characters, index, "\n")
+    end
+
+    local string = table.concat(characters)
+    love.filesystem.setIdentity("Points")
+    love.filesystem.write("points.txt", string)
+  elseif key == "p" then
+    love.filesystem.setIdentity("Points")
+    love.graphics.captureScreenshot("points.png")
+  elseif key == "escape" then
+    love.event.quit()
+  end
+end
+
+function love.update(dt)
+  if love.mouse.isDown(1) then
+    local x, y = love.mouse:getPosition()
+    local column = math.floor(x / CELL_SIZE) + 1
+    local row = math.floor(y / CELL_SIZE) + 1
+
+    if not grid:hasPoint(column, row) then
+      grid:addPoint(column, row)
+    end
+  end
+
+  if love.mouse.isDown(2) then
+    local x, y = love.mouse:getPosition()
+    local column = math.floor(x / CELL_SIZE) + 1
+    local row = math.floor(y / CELL_SIZE) + 1
+
+    if grid:hasPoint(column, row) then
+      grid:removePoint(column, row)
+    end
+  end
+end
+
+function love.draw()
+  grid:render()
+end
